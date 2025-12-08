a98996df24f5fb91f58683190a16eccc50ac3d18
AO031 <3034089412@qq.com>
2025-12-08 09:57:08 +0800

    feat:x64 Shellcode  更新了栈区变量，更清晰了

--- HideModule/AsmShellcode.asm
+++ HideModule/AsmShellcode.asm
@@ -17,9 +17,63 @@ Shellcode PROC
     push r9
     mov rbp, rsp
     
-    sub rsp, 40h
+    sub rsp, 120h
     and rsp, -10h
 
+    mov byte ptr [rsp+20h], "L"
+    mov byte ptr [rsp+21h], "o"
+    mov byte ptr [rsp+22h], "a"
+    mov byte ptr [rsp+23h], "d"
+    mov byte ptr [rsp+24h], "L"
+    mov byte ptr [rsp+25h], "i"
+    mov byte ptr [rsp+26h], "b"
+    mov byte ptr [rsp+27h], "r"
+    mov byte ptr [rsp+28h], "a"
+    mov byte ptr [rsp+29h], "r"
+    mov byte ptr [rsp+2Ah], "y"
+    mov byte ptr [rsp+2Bh], "A"
+    mov byte ptr [rsp+2Ch], 0
+
+    mov byte ptr [rsp+2Dh], "G"
+    mov byte ptr [rsp+2Eh], "e"
+    mov byte ptr [rsp+2Fh], "t"
+    mov byte ptr [rsp+30h], "P"
+    mov byte ptr [rsp+31h], "r"
+    mov byte ptr [rsp+32h], "o"
+    mov byte ptr [rsp+33h], "c"
+    mov byte ptr [rsp+34h], "A"
+    mov byte ptr [rsp+35h], "d"
+    mov byte ptr [rsp+36h], "d"
+    mov byte ptr [rsp+37h], "r"
+    mov byte ptr [rsp+38h], "e"
+    mov byte ptr [rsp+39h], "s"
+    mov byte ptr [rsp+3Ah], "s"
+    mov byte ptr [rsp+3Bh], 0
+
+    mov byte ptr [rsp+3Ch], "U"
+    mov byte ptr [rsp+3Dh], "s"
+    mov byte ptr [rsp+3Eh], "e"
+    mov byte ptr [rsp+3Fh], "r"
+    mov byte ptr [rsp+40h], "3"
+    mov byte ptr [rsp+41h], "2"
+    mov byte ptr [rsp+42h], "."
+    mov byte ptr [rsp+43h], "d"
+    mov byte ptr [rsp+44h], "l"
+    mov byte ptr [rsp+45h], "l"
+    mov byte ptr [rsp+46h], 0
+
+    mov byte ptr [rsp+47h], "M"
+    mov byte ptr [rsp+48h], "e"
+    mov byte ptr [rsp+49h], "s"
+    mov byte ptr [rsp+4Ah], "s"
+    mov byte ptr [rsp+4Bh], "a"
+    mov byte ptr [rsp+4Ch], "g"
+    mov byte ptr [rsp+4Dh], "e"
+    mov byte ptr [rsp+4Eh], "B"
+    mov byte ptr [rsp+4Fh], "o"
+    mov byte ptr [rsp+50h], "x"
+    mov byte ptr [rsp+51h], "A"
+    mov byte ptr [rsp+52h], 0
 
     mov rdi, qword ptr gs:[30h] ;TEB
     mov rdi, [rdi + 60h] ;PEB
@@ -70,94 +124,63 @@ FindSuccess:
     add r12, r10
     add r13, r10
 
-    ; ==== Loadlib ====
-    sub rsp, 60h
 
-    mov byte ptr [rsp+0h], "L"
-    mov byte ptr [rsp+1h], "o"
-    mov byte ptr [rsp+2h], "a"
-    mov byte ptr [rsp+3h], "d"
-    mov byte ptr [rsp+4h], "L"
-    mov byte ptr [rsp+5h], "i"
-    mov byte ptr [rsp+6h], "b"
-    mov byte ptr [rsp+7h], "r"
-    mov byte ptr [rsp+8h], "a"
-    mov byte ptr [rsp+9h], "r"
-    mov byte ptr [rsp+0ah], "y"
-    mov byte ptr [rsp+0bh], "A"
-    mov byte ptr [rsp+0ch], 0
-    mov byte ptr [rsp+0dh], "G"
-    mov byte ptr [rsp+0eh], "e"
-    mov byte ptr [rsp+0fh], "t"
-    mov byte ptr [rsp+10h], "P"
-    mov byte ptr [rsp+11h], "r"
-    mov byte ptr [rsp+12h], "o"
-    mov byte ptr [rsp+13h], "c"
-    mov byte ptr [rsp+14h], "A"
-    mov byte ptr [rsp+15h], "d"
-    mov byte ptr [rsp+16h], "d"
-    mov byte ptr [rsp+17h], "r"
-    mov byte ptr [rsp+18h], "e"
-    mov byte ptr [rsp+19h], "s"
-    mov byte ptr [rsp+1ah], "s"
-    mov byte ptr [rsp+1bh], 0
-
-    mov qword ptr [rsp+20h], 0 ; bLoadRes
-    mov qword ptr [rsp+28h], 0 ; bGetRes
-    mov qword ptr [rsp+30h], rcx
+    mov qword ptr [rsp+60h], 0 ; bLoadRes
+    mov qword ptr [rsp+68h], 0 ; bGetRes
+    mov qword ptr [rsp+70h], rcx ;NumberOfFuncations
 
 
     xor rbx, rbx
     
     ; while rbx <= rcx
 StartFindTwo:
-    cmp rbx, [rsp+30h]
+    cmp rbx, [rsp+70h]
     ja findLGLoop
 
 StartFindLoadLibraryA:
     mov ecx, dword ptr [r12+rbx*4h]
     add rcx, r10
-    lea rdx, [rsp+0h]
+    lea rdx, [rsp+20h]
     call MyStrcmp
     cmp rax, 0
     jne StartFindGetProcAddress
 findLoadSuccess:
-    mov qword ptr [rsp+20h], 1
-    mov qword ptr [rsp+38h], rbx ; Load idx in Export
+    mov qword ptr [rsp+60h], 1
+    mov qword ptr [rsp+78h], rbx ; Load idx in Export
 
 
 StartFindGetProcAddress:
     mov ecx, dword ptr [r12+rbx*4h]
     add rcx, r10
-    lea rdx, [rsp+0dh]
+    lea rdx, [rsp+2Dh]
     call MyStrcmp
     cmp rax, 0
     jne CheckBothFound
 
 findGetSuccess:
-    mov qword ptr [rsp+28h], 1
-    mov qword ptr [rsp+40h], rbx ; Get idx in Export
+    mov qword ptr [rsp+68h], 1
+    mov qword ptr [rsp+80h], rbx ; Get idx in Export
 
 CheckBothFound:
     inc rbx
-    cmp qword ptr [rsp+20h], 1
+    cmp qword ptr [rsp+60h], 1
     jne StartFindTwo
-    cmp qword ptr [rsp+28h], 1
+    cmp qword ptr [rsp+68h], 1
     jne StartFindTwo
 
     
 
 findLGLoop:
-    cmp qword ptr [rsp+20h], 1
+    cmp qword ptr [rsp+60h], 1
     jne ExitFindLoadAndGet
-    mov rbx, qword ptr [rsp+38h]
+    mov rbx, qword ptr [rsp+78h]
     movzx rdi, word ptr [r13+rbx*2h]
     mov edi, dword ptr [r11+rdi*4h]
     add rdi, r10 
     mov r8, rdi ;LoadLibraryA
-    cmp qword ptr [rsp+28h], 1
+    cmp qword ptr [rsp+68h], 1
     jne ExitFindLoadAndGet
-    mov rbx, qword ptr [rsp+40h]
+    mov rbx, qword ptr [rsp+80h]
     movzx rdi, word ptr [r13+rbx*2h]
     mov edi, dword ptr [r11+rdi*4h]
     add rdi, r10 
@@ -166,49 +189,20 @@ findLGLoop:
 
 
 ExitFindLoadAndGet:
-
-    add rsp, 60h
-    ; --- 在栈上构造字符串 "User32.dll" (偏移从 0x20 开始，留在影子空间之后) ---
-    mov byte ptr [rsp+20h], "U"
-    mov byte ptr [rsp+21h], "s"
-    mov byte ptr [rsp+22h], "e"
-    mov byte ptr [rsp+23h], "r"
-    mov byte ptr [rsp+24h], "3"
-    mov byte ptr [rsp+25h], "2"
-    mov byte ptr [rsp+26h], "."
-    mov byte ptr [rsp+27h], "d"
-    mov byte ptr [rsp+28h], "l"
-    mov byte ptr [rsp+29h], "l"
-    mov byte ptr [rsp+2Ah], 0
-    mov byte ptr [rsp+2Bh], "M"   ; 第二个参数：指向 "MessageBoxA"
-    mov byte ptr [rsp+2Ch], "e"
-    mov byte ptr [rsp+2Dh], "s"
-    mov byte ptr [rsp+2Eh], "s"
-    mov byte ptr [rsp+2Fh], "a"
-    mov byte ptr [rsp+30h], "g"
-    mov byte ptr [rsp+31h], "e"
-    mov byte ptr [rsp+32h], "B"
-    mov byte ptr [rsp+33h], "o"
-    mov byte ptr [rsp+34h], "x"
-    mov byte ptr [rsp+35h], "A"
-    mov byte ptr [rsp+36h], 0
-    mov qword ptr [rsp+40h], r8
-    mov qword ptr [rsp+48h], r9
+    mov qword ptr [rsp+88h], r8
+    mov qword ptr [rsp+90h], r9
 
     ; --- 调用 LoadLibraryA("User32.dll") ---
-    lea rcx, [rsp+20h]              ; 第一个参数：指向 "User32.dll"
-    mov r8, qword ptr [rsp+40h]
+    lea rcx, [rsp+3Ch]              ; 第一个参数：指向 "User32.dll"
+    mov r8, qword ptr [rsp+88h]
     call r8                         ; 调用 LoadLibraryA (地址在R8中)
                                     ; 此时 rsp 指向分配的开始位置，其后的0x20字节就是预留的影子空间
 
     ; --- 调用 GetProcAddress(hUser32, "MessageBoxA") ---
     mov rcx, rax                    ; 第一个参数：User32.dll的模块句柄 (来自上一步的RAX)
-    lea rdx, [rsp+2Bh]              ; 第二个参数：指向 "MessageBoxA"
-    mov r9, qword ptr [rsp+48h]
+    lea rdx, [rsp+47h]              ; 第二个参数：指向 "MessageBoxA"
+    mov r9, qword ptr [rsp+90h]
     call r9                         ; 调用 GetProcAddress (地址在R9中)
-
-    ; --- 清理栈空间并返回 ---
-    add rsp, 60h                    ; 恢复栈指针
      
     ; rax 现在存放 MessageBoxA 的地址
     xor rcx, rcx
